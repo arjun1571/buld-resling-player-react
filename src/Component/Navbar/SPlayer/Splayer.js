1609b35b6e0e1d350b286de2f34013720a39e3b9
Arjun Das <arjundasback515@gmail.com>
2023-02-15 12:50:23 +0530

    button added

--- src/Component/Navbar/SPlayer/Splayer.js
+++ src/Component/Navbar/SPlayer/Splayer.js
@@ -7,6 +7,9 @@ const Splayer = ({pl}) => {
         <div className='cart-d'>
             <img className='img-style' src={pl?.strThumb} alt="" />
             <h3>{pl.strBirthLocation}</h3>
+            <button className='buttton-cart'>Detail</button>
+            <button className='buttton-cart'>Add to cart</button>
+            <button className='buttton-cart'>Bookmark</button>
         </div>
     );
 };
